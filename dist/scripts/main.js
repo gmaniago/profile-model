e9345519e46e1671816ae983bf06e05ad87a2040
Gladys Maniago <gfmaniago@gmail.com>
2015-09-22 15:19:02 -0500

    first push

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,18 +1,17 @@
 var user = new UserModel();
-var App = Backbone.Router.extend({
-	routes: {
-		'': 'profile',
-		'edit': 'edit'
-	},
-	profile: function() {
-		$('.page').hide();
-		$('#profile').show();
-	},
-	edit: function() {
-		$('.page').hide();
-		$('#edit').show();
-	}
+
+$(document).ready(function() {
+
+
+$('.form-horizontal').on('submit', function(e) {
+	e.preventDefault();
+	user.set({
+		name: $('#name').val(),
+		email: $('#inputEmail3').val(),
+		role: $('#role').val()
+	});
+
 });
 
-var app = new App();
-Backbone.history.start();
+
+});
